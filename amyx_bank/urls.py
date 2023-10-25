84348af509b1f6f6da039e672846f60542fc1ca9
ElPayo <elpayov2@gmail.com>
2023-10-25 23:26:16 +0100

    files

---
+++ amyx_bank/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
