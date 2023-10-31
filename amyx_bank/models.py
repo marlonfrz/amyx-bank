0812dba6e2ea505525703d38f28300bf186b8a83
ElPayo <elpayov2@gmail.com>
2023-10-31 00:20:51 +0000

    new app added

--- amyx_bank/models.py
+++ amyx_bank/models.py
@@ -20,7 +20,7 @@ class BankAccount(models.Model):
 
     account_name = models.CharField(max_length=50, primary_key=False)
     account_balance = models.DecimalField(max_digits=7, decimal_places=2, default=0)
-    account_code = models.CharField(max_length=20, null=False, blank=True)
+    account_code = models.CharField(max_length=20, null=False, blank=True, default="A5-0001")
     status = models.CharField(max_length=20, default=Status.ACTIVE, choices=Status.choices)
     objects = models.Manager()
 
@@ -33,15 +33,11 @@ class BankAccount(models.Model):
     # NO TOCAR EL METODO SAVE; FUNCIONA BIEN
     def save(self, *args, **kwargs) -> None:
         bank_prefix = "A5"
-        try:
-            last_used_account_code = BankAccount.objects.latest("account_code").account_code
-            new_bank_account_code = (
-                f"{bank_prefix}-{int(last_used_account_code[3:].lstrip('0')) + 1:04d}"
-            )
-            print("hasta aqui llega")
-            self.account_code = new_bank_account_code
-        except BankAccount.DoesNotExist:
-            self.account_code = "A5-0001"
+        last_used_account_code = BankAccount.objects.latest("account_code").account_code
+        new_bank_account_code = (
+            f"{bank_prefix}-{int(last_used_account_code[3:].lstrip('0')) + 1:04d}"
+        )
+        self.account_code = new_bank_account_code
         return super(__class__, self).save(*args, **kwargs)
 
     # adaptar y crear view de account_detail
