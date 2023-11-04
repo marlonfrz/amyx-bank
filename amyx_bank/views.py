bffa4ec02d5dafb2c2a23239f2486a2f90d901d9
ElPayo <elpayov2@gmail.com>
2023-11-04 15:47:43 +0000

    vistas

--- amyx_bank/views.py
+++ amyx_bank/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from .forms import BankAccountForm, LoginForm, Profile, UserRegistrationForm, Edit
+from .forms import BankAccountForm, LoginForm, Profile, UserRegistrationForm,
 
 
 def register(request):
