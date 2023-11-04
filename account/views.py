b0f3fcf83317684a025341c59aeb66f4b74a23e9
ElPayo <elpayov2@gmail.com>
2023-11-04 16:21:42 +0000

    cambio de la estructura del proyecto

--- account/views.py
+++ account/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import UserEditForm, BankAccountForm
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+@login_required
+def edit_account(request):
+    pass
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserEditForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserEditForm(instance=request.user)
+    return render(request, 'edit_profile.html', {'user_edit_form': form})
+
+@login_required
+def bank_account_create_view(request):
+    if request.method == 'POST':
+        bank_account_form = BankAccountForm(request.POST)
+        if bank_account_form.is_valid():
+            bank_account_form.save()
+            return redirect('account_create_success')
+    else:
+        form = BankAccountForm()
+    return render(request, 'amyx_bank/account_create.html', {'bank_account_create_form': form})
+
+
+@login_required
+def card_create_view(request):
+    pass
+
+
+
+
+def account_create_success(request):
+    return render(request, 'amyx_bank/account_create_done.html')
+
+
+
+@login_required
+def edit_card(request):
+    pass
--- amyx_bank/views.py
+++ amyx_bank/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from .forms import BankAccountForm, LoginForm, Profile, UserRegistrationForm
+from account.models import Profile
+from .forms import LoginForm
+from account.forms import UserRegistrationForm
 
 
 def register(request):
@@ -40,43 +42,6 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'registration/login.html', {'form': form})
-
-
-@login_required
-def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
-
-
-@login_required
-def bank_account_create_view(request):
-    if request.method == 'POST':
-        bank_account_form = BankAccountForm(request.POST)
-        if bank_account_form.is_valid():
-            bank_account_form.save()
-            # Redirige a la página de inicio del tablero o donde desees después de crear la cuenta
-            return redirect('account_create_success')
-    else:
-        form = BankAccountForm()
-    return render(request, 'amyx_bank/account_create.html', {'bank_account_create_form': form})
-
-
-@login_required
-def card_create_view(request):
-    pass
-
-
-@login_required
-def edit_account(request):
-    pass
-
-
-@login_required
-def edit_card(request):
-    pass
-
-
-def account_create_success(request):
-    return render(request, 'amyx_bank/account_create_done.html')
 
 
 def logout(request):
