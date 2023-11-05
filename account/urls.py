a55da87f306e2b95bc8651ef6357939f01e3e211
ElPayo <elpayov2@gmail.com>
2023-11-05 12:20:13 +0000

    utils modificado, views creadas, formularios creados

--- account/urls.py
+++ account/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('new_bank_account/', views.bank_account_create_view, name='new_account'),
     path('edit/account', views.edit_account, name='edit_account'),
     path('edit/card/', views.edit_card, name='edit_card'),
     path('edit/profile/', views.edit_profile, name='edit_profile'),
     path('account_create_success/', views.account_create_success, name='account_create_success'),
+    path('card_detail/', views.card_create_view, name='card_detail'),
 ]
+
