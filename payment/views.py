93de82b1e1755937eb17ecf8111f5d0f587520a9
Marlon <marlonfh2b1112@gmail.com>
2023-11-15 10:20:54 +0000

    tocando la vista y templates movements

--- payment/views.py
+++ payment/views.py
@@ -13,9 +13,10 @@ from amyx_bank.ourutils import calc_commission, get_bank_info
 import json, requests
 from decimal import Decimal
 
+
 @csrf_exempt
 def payment(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             cd = json.loads(request.body)
         except json.JSONDecodeError:
@@ -23,13 +24,15 @@ def payment(request):
             if form.is_valid():
                 cd = form.cleaned_data
         else:
-            return HttpResponseBadRequest("Payment was cancelled due to invalid information passed in the POST request")
-        business = cd.get('business').upper()
-        ccc = cd.get('ccc').upper()
-        amount = Decimal(cd.get('amount'))
+            return HttpResponseBadRequest(
+                "Payment was cancelled due to invalid information passed in the POST request"
+            )
+        business = cd.get("business").upper()
+        ccc = cd.get("ccc").upper()
+        amount = Decimal(cd.get("amount"))
         if amount == 0:
             return HttpResponseBadRequest("You cannot send no money to anyone!")
-        pin = cd.get('pin')
+        pin = cd.get("pin")
         try:
             card = Card.objects.get(card_code=ccc)
         except Card.DoesNotExist:
@@ -48,18 +51,21 @@ def payment(request):
                 )
                 return HttpResponse("Ok!")
             else:
-                return HttpResponseBadRequest("Everything went ok, but you don't have enough money")
+                return HttpResponseBadRequest(
+                    "Everything went ok, but you don't have enough money"
+                )
         else:
             return HttpResponseForbidden(f"The code pin {pin} doesn't match")
     else:
         form = PaymentForm()
-    return render(request, 'payment/payment.html', {'payment_form' : form})
+    return render(request, "payment/payment.html", {"payment_form": form})
+
 
 @csrf_exempt
 def outgoing_transactions(request):
-    # Este bloque controla que los datos pueden 
+    # Este bloque controla que los datos pueden
     # llegar tanto por formulario como por curl
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             cd = json.loads(request.body)
         except json.JSONDecodeError:
@@ -67,19 +73,21 @@ def outgoing_transactions(request):
             if form.is_valid():
                 cd = form.cleaned_data
         else:
-            return HttpResponseBadRequest("Transaction was cancelled due to invalid information passed in the POST request")
+            return HttpResponseBadRequest(
+                "Transaction was cancelled due to invalid information passed in the POST request"
+            )
         # Obtención los datos del diccionario
-        sender = cd.get('sender').upper()
-        account = cd.get('cac').upper()
+        sender = cd.get("sender").upper()
+        account = cd.get("cac").upper()
         # Clausulas guarda para despachar los errores posibles
         if sender == account:
             return HttpResponse("You can't send money to yourself")
-        amount = Decimal(cd.get('amount'))
+        amount = Decimal(cd.get("amount"))
         if amount == 0:
             return HttpResponse("You can't send no money to anyone")
-        
+
         concept = cd.get("concept")
-        
+
         destined_bank = get_bank_info(account, "url")
         # Obtiene la cuenta del sender
         try:
@@ -89,28 +97,35 @@ def outgoing_transactions(request):
         taxed_amount = amount + calc_commission(amount, "OUTGOING")
         print(taxed_amount)
         if cac.balance > taxed_amount:
-            transaction = {"agent" : sender, "account" : account, "concept" : concept, "amount" : amount}
+            transaction = {
+                "agent": sender,
+                "account": account,
+                "concept": concept,
+                "amount": amount,
+            }
             status = requests.post(f"http://127.0.0.1:8000/incoming/", transaction)
-            #print(status)
+            # print(status)
             cac.balance -= taxed_amount
             cac.save()
             Transaction.objects.create(
-                    agent=sender,
-                    account=account,
-                    concept=concept,
-                    amount=amount,
-                )
+                agent=sender,
+                account=account,
+                concept=concept,
+                amount=amount,
+            )
             return HttpResponse("Everything went ok with the transaction")
-        return HttpResponse("todo ha ido bien, solo falta que implementes las nominas")  # QUITAR CUANDO TODO FUNCIONE
+        return HttpResponse(
+            "todo ha ido bien, solo falta que implementes las nominas"
+        )  # QUITAR CUANDO TODO FUNCIONE
     else:
         form = TransactionForm()
-    return render(request, 'payment/transactions.html', {'transaction_form' : form})
+    return render(request, "payment/transactions.html", {"transaction_form": form})
 
 
 @login_required
 @require_POST
 def incoming_transactions(request):
-    # Este bloque controla que los datos pueden 
+    # Este bloque controla que los datos pueden
     # llegar tanto por formulario como por curl
     try:
         cd = json.loads(request.body)
@@ -120,61 +135,53 @@ def incoming_transactions(request):
             cd = form.cleaned_data
         else:
             return HttpResponse("Invalid form data format")
-        
+
         # Obtención los datos del diccionario
-        sender = cd.get('sender')
-        cac = cd.get('cac')
-        concept = cd.get('concept')
-        amount = Decimal(cd.get('amount'))
+        sender = cd.get("sender")
+        cac = cd.get("cac")
+        concept = cd.get("concept")
+        amount = Decimal(cd.get("amount"))
 
         # Comprueba que la cuenta existe
         try:
             account = BankAccount.objects.get(account_code=cac)
         except BankAccount.DoesNotExist:
-            return HttpResponseBadRequest("The account you tried to send money to does not exist")
-        
-        taxed_amount = calc_commission(amount, 'INCOMING')
+            return HttpResponseBadRequest(
+                "The account you tried to send money to does not exist"
+            )
+
+        taxed_amount = calc_commission(amount, "INCOMING")
         total_amount = amount - taxed_amount
         account.balance += total_amount
         account.save()
-        new_transaction = Transaction.objects.create(agent=sender, account=account, concept=concept, amount=amount)
+        new_transaction = Transaction.objects.create(
+            agent=sender, account=account, concept=concept, amount=amount
+        )
         return redirect("dashboard")
+
 
 @csrf_exempt
 def payroll(request):
-    # Los campos de payroll son unicamente la cuenta destino y 
+    # Los campos de payroll son unicamente la cuenta destino y
     # La cantidad de dinero a instroducir
     # cuyos nombres seran cac y balance respectivamente
     cd = json.loads(request.body)
     print(cd)
-    balance = Decimal((cd.get('balance')))
+    balance = Decimal((cd.get("balance")))
     print(balance)
-    account = get_object_or_404(BankAccount, account_code=cd.get('cac').upper())
+    account = get_object_or_404(BankAccount, account_code=cd.get("cac").upper())
     print(account)
     account.balance += balance
     account.save()
     return HttpResponse("NOMINA BIEN METIDA PARA DENTRO")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def movements(request):
+    transactions = Transaction.objects.all()
+    payments = Payment.objects.all()
+    return render(
+        request, "movements.html", {"transactions": transactions, "payment": payments}
+    )
 
 
 @login_required
