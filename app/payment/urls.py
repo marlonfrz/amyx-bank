db737fe898eb9acd357b6c39f93b591f24c924e4
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-12-30 13:15:22 +0000

    Se empieza a traducir las plantillas

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 from . import views
 
 urlpatterns = [
     path('', views.payment, name='payments'),
-    path('payment_success/', views.payment_success, name='payment_success'),
-    path('payroll/', views.payroll, name='payroll'),
-    path('movements/', views.movements, name='movements'),
-    path('payment_detail/<int:id>', views.payment_detail, name='payment_detail'),
-    path('trasaction_detail/<int:id>', views.transaction_detail, name='transaction_detail'),
-    path('trasaction_detail/<int:id>/pdf/',views.transaction_pdf, name='transaction_pdf'),
-    path('payment_detail/<int:id>/pdf/',views.payment_pdf, name='payment_pdf'),
-
+    path(_('payment_success/'), views.payment_success, name='payment_success'),
+    path(_('payroll/'), views.payroll, name='payroll'),
+    path(_('movements/'), views.movements, name='movements'),
+    path(_('payment_detail/<int:id>'), views.payment_detail, name='payment_detail'),
+    path(_('trasaction_detail/<int:id>'), views.transaction_detail, name='transaction_detail'),
+    path(_('trasaction_detail/<int:id>/pdf/'), views.transaction_pdf, name='transaction_pdf'),
+    path(_('payment_detail/<int:id>/pdf/'), views.payment_pdf, name='payment_pdf'),
 ]
