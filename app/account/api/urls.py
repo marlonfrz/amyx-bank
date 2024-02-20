263f4b66fb8fb1a1afc3999a1699ab7f0385c484
marlonfrz <marlonfh2b1112@gmail.com>
2024-02-20 22:28:57 +0100

    limpieza de ficheros

--- app/account/api/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from . import views
-#
-# app_name = 'account'
-#
-# urlpatterns = [
-#     path('accounts/',views.BankAccountListView.as_view(), name='account_list'),
-#     path('accounts/<pk>/', views.BankAccountDetailView.as_view(), name='account_detail'),
-# ]
